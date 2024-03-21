15e8e7296a2de71002938279986577899ccc9f00
amal2000-cse <kannothparambathamalmanoj@gmail.com>
2024-03-21 19:11:47 +0530

    updated .env

--- backend/db/connectToDB.js
+++ backend/db/connectToDB.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectToMongoDB = async()=>{
     try {
-        // console.log(process.env.MONGOURL)
+        // console.log(process.env.trial)
         await mongoose.connect(process.env.MONGOURL)
         console.log('Connected to MongoDB')
     } catch (error) {
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,34 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
+import path from "path";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToDB.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
-
 dotenv.config();
+// console.log(process.env);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json()); //to parse the imcoming requests with JSON payloaads (from req.body)
 app.use(cookieParser()); //to use cookie parser
-app.get("/", (req, res) => {
-  res.send("Welcome to ChatApp 2.0!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Welcome to ChatApp 2.0!");
+// });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
